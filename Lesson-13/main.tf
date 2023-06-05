9d5ca00a55ce4c58f3debf245a2fa6814922381c
Andrei Belyi <andreybelyyw@gmail.com>
2023-06-05 12:39:39 -0600

    Lesson - 13

--- Lesson-13/main.tf
+++ Lesson-13/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
 
   default_tags {
     tags = {
@@ -11,24 +11,31 @@ provider "aws" {
 }
 #------------------
 data "aws_availability_zones" "working" {}
+
 data "aws_ami" "latest_alinux" {
-  owners      = ["137112412989"]
+  owners      = ["amazon"]
   most_recent = true
   filter {
     name   = "name"
     values = ["amzn2-ami-kernel-5.10-hvm-2.0.*"]
   }
 }
+
+resource "aws_instance" "server" {
+  ami                    = data.aws_ami.latest_alinux.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web.id]
+  monitoring             = var.detailed_monitoring
+}
 #-----------------
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.server.id
+  #tags     = var.common-tags
+  tags = merge(var.common-tags, { Name = "${var.common-tags["Environment"]} Server IP" })
+}
 
 resource "aws_default_vpc" "default" {}
 
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = data.aws_availability_zones.working.names[0]
-}
-resource "aws_default_subnet" "default_az2" {
-  availability_zone = data.aws_availability_zones.working.names[1]
-}
 #------------------
 resource "aws_security_group" "web" {
   name        = "Dynamic SG"
@@ -36,7 +43,7 @@ resource "aws_security_group" "web" {
   vpc_id      = aws_default_vpc.default.id
 
   dynamic "ingress" {
-    for_each = ["80", "443"]
+    for_each = var.inbound_ports
     content {
       from_port   = ingress.value
       to_port     = ingress.value
@@ -52,16 +59,14 @@ resource "aws_security_group" "web" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  tags = merge(var.common-tags, { Name = "${var.common-tags["Environment"]} Web Security Group" })
 
-  tags = {
-    Name = "Web Security Group"
-  }
 }
 #------------------
 
+#------------------
 
 #------------------
 
 
 #------------------
-
