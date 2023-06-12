380b7f4f2d06a5e5ccd4dcc755860d00adfb4b61
Andrei Belyi <andreybelyyw@gmail.com>
2023-06-12 14:08:30 -0600

    Lesson 25 (Replace)

--- Lesson-25/main.tf
+++ Lesson-25/main.tf
@@ -2,33 +2,33 @@
 # terraform apply -replace aws_instance.node2
 #
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "node1" {
   ami           = "ami-04a0ae173da5807d3"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags = {
     Name  = "Node-1"
-    Owner = "Denis Astahov"
+    Owner = "Andrei Belyi"
   }
 }
 
 resource "aws_instance" "node2" {
   ami           = "ami-04a0ae173da5807d3"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags = {
     Name  = "Node-2"
-    Owner = "Denis Astahov"
+    Owner = "Andrei Belyi"
   }
 }
 
 resource "aws_instance" "node3" {
   ami           = "ami-04a0ae173da5807d3"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags = {
     Name  = "Node-3"
-    Owner = "Denis Astahov"
+    Owner = "Andrei Belyi"
   }
   depends_on = [aws_instance.node2]
 }
