038e7c7bf84eb0e69c860f1c0457d826c288bfcc
Andrei Belyi <53856792+AndreyBelyy@users.noreply.github.com>
2023-08-21 16:29:36 -0600

    Update main.tf

--- project_with_jenkins/main.tf
+++ project_with_jenkins/main.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+    region = "us-east-1"
+}
 
+resource "aws_instance" "arfa" {
+    ami             = "ami-053b0d53c279acc90"
+    instance_type   = "t2.micro"
+    tags = {
+        Name = "jandt"
+    }
+}
+
+terrafrom {
+    backend "s3" {
+        bucket = "devopsprojectaw"
+        key    = "terraform.tfstate"
+        region = "us-east-1"
+    }
